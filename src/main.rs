0cfa90419f8a10d4fe11176241d3dc72971092a7
chachacollins <collinschacha@hotmail.com>
2025-06-19 19:52:51 +0300

    batman

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lex_parens() {
+        let source = "(){}";
+        assert_eq!();
+    }
+}
