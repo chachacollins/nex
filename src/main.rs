85b4eec302a711f0dafa7fb1a84c828e584617e5
chachacollins <collinschacha@hotmail.com>
2025-06-23 17:59:59 +0300

    style: made the repl prettier

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,38 @@ mod vm;
 
 use miette::Result;
 fn repl() -> Result<()> {
+    const GREEN: &str = "\x1b[32m";
+    const RED: &str = "\x1b[31m";
+    const RESET: &str = "\x1b[0m";
+    let mut success = true;
     loop {
         let mut source = String::new();
-        print!(">> ");
+        if success {
+            print!("{}>>{} ", GREEN, RESET);
+        } else {
+            print!("{}>>{} ", RED, RESET);
+        }
         stdout().flush().unwrap();
         match stdin().read_line(&mut source) {
-            Ok(_) => {
-                let chunk = compiler::compile(source.clone())?;
-                let mut vm = Vm::new(source, chunk);
-                let result = vm.eval()?;
-                println!("{result}")
-            }
+            Ok(_) => match compiler::compile(source.clone()) {
+                Ok(chunk) => {
+                    let mut vm = Vm::new(source, chunk);
+                    match vm.eval() {
+                        Ok(result) => {
+                            println!("{result}");
+                            success = true;
+                        }
+                        Err(error) => {
+                            eprintln!("{:?}", error);
+                            success = false;
+                        }
+                    }
+                }
+                Err(error) => {
+                    eprintln!("{:?}", error);
+                    success = false;
+                }
+            },
             Err(error) => println!("error: {error}"),
         }
     }
